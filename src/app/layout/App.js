9c857a6e7302c3bc4ef575fcc81be2346254de4f
dm1tro69 <dimolg22@gmail.com>
2021-10-03 21:48:28 +0300

    initial commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,12 @@
+
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Re-events</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/layout/App';
+
+
+const rootEl = document.getElementById('root')
+
+function render() {
+    ReactDOM.render(<App/>, rootEl)
+}
+if (module.hot){
+    module.hot.accept('./app/layout/App', function () {
+        setTimeout(render)
+    })
+}
+render()
+
+
