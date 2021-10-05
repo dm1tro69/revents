c6b657e6ddcc35881d06da7cb81e25ba50b2ea81
dm1tro69 <dimolg22@gmail.com>
2021-10-05 22:25:29 +0300

    add routing

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -3,7 +3,11 @@ import EventDashboard from "../../features/events/eventDashboard/EventDashboard"
 import NavBar from "../../features/nav/NavBar";
 import './styles.css';
 import {Container} from "semantic-ui-react";
+import {Route, Switch} from "react-router-dom";
 import {useState} from "react";
+import HomePage from "../../features/home/HomePage";
+import EventDetailedPage from "../../features/events/eventDetailed/EventDetailedPage";
+import EventForm from "../../features/events/eventForm/EventForm";
 
 function App() {
     const [formOpen, SetFormOpen] = useState(false)
@@ -22,11 +26,25 @@ function App() {
     <>
      <NavBar SetFormOpen={handleCreateFormOpen} formOpen={formOpen}/>
         <Container className={'main'}>
-            <EventDashboard
-                formOpen={formOpen}
-                SetFormOpen={SetFormOpen}
-                selectedEvent={selectedEvent}
-                handleSelectEvent={handleSelectEvent}/>
+            <Switch>
+                <Route exact path={'/'}>
+                    <HomePage/>
+                </Route>
+                <Route path={'/events/:id'}>
+                    <EventDetailedPage/>
+                </Route>
+                <Route exact path={'/events'}>
+                    <EventDashboard
+                        formOpen={formOpen}
+                        SetFormOpen={SetFormOpen}
+                        selectedEvent={selectedEvent}
+                        handleSelectEvent={handleSelectEvent}/>
+                </Route>
+                <Route path={'/createEvent'}>
+                    <EventForm/>
+                </Route>
+
+            </Switch>
         </Container>
 
     </>
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './app/layout/App';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
+import {BrowserRouter} from "react-router-dom";
 
 
 
 
-ReactDOM.render(<App />,
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
+    ,
     document.getElementById('root')
 );
 
